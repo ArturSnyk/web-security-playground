ffb70f6df2cdc3c9c2a4bcc97fb0f7e8b3b81f67
Artur Kotliar <artur@snyk.io>
2020-03-28 19:46:12 +0300

    add servers with cookie support

--- server3/server.ts
+++ server3/server.ts
@@ -4,21 +4,29 @@ import { createReadStream } from 'fs';
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 
+const COOKIE_SECRET = 'not_so_random_string';
+
 const USERS = {
   alice: 'pass',
   bob: 'achu'
 };
 
+const BALANCE = {
+  alice: 500,
+  bob: 100
+};
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 app.listen(4000);
 
 app.get('/', (req, res) => {
-  const username = req.cookies.username;
+  const username = req.signedCookies.username;
   if (username) {
-    res.send(`Hi ${username}!`);
+    const balance = BALANCE[username];
+    res.send(`Hi ${username}. Your balance is ${balance}$`);
   } else {
     // res.sendFile(path.join(__dirname + '/../../server2/index.html'));
     createReadStream('./server2/index.html').pipe(res);
@@ -30,10 +38,15 @@ app.post('/login', (req, res) => {
   const password: string | undefined = USERS[username];
 
   if (req.body.password === password) {
-    res.cookie('username', req.body.username);
+    res.cookie('username', req.body.username, { signed: true });
     res.send('nice!')
   } else {
     res.send('fail!');
   }
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/');
 });
 
--- server4/server.ts
+++ server4/server.ts
@@ -3,22 +3,33 @@ import * as path from 'path';
 import { createReadStream } from 'fs';
 import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
+import { randomBytes } from 'crypto';
+
+const COOKIE_SECRET = 'not_so_random_string';
 
 const USERS = {
   alice: 'pass',
   bob: 'achu'
 };
 
+const BALANCE = {
+  alice: 500,
+  bob: 100
+};
+const SESSIONS = {}; //sessionId -> username
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser());
+app.use(cookieParser(COOKIE_SECRET));
 app.listen(4000);
 
 app.get('/', (req, res) => {
-  const username = req.cookies.username;
+  const sessionId = req.cookies.sessionId;
+  const username = SESSIONS[sessionId];
   if (username) {
-    res.send(`Hi ${username}!`);
+    const balance = BALANCE[username];
+    res.send(`Hi ${username}. Your balance is ${balance}$`);
   } else {
     // res.sendFile(path.join(__dirname + '/../../server2/index.html'));
     createReadStream('./server2/index.html').pipe(res);
@@ -30,10 +41,19 @@ app.post('/login', (req, res) => {
   const password: string | undefined = USERS[username];
 
   if (req.body.password === password) {
-    res.cookie('username', req.body.username);
+    const nextSessionsId = randomBytes(16).toString('base64');
+    res.cookie('sessionId', nextSessionsId);
+    SESSIONS[nextSessionsId] = username;
     res.send('nice!')
   } else {
     res.send('fail!');
   }
+});
+
+app.get('/logout', (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  delete SESSIONS[sessionId];
+  res.clearCookie('username');
+  res.redirect('/');
 });
 
